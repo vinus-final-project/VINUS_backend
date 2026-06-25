569dec6354440eea15ab6dee4597a8afd5f81a58
chaewon <sb06266@naver.com>
2026-06-25 16:34:41 +0900

    라우터 다시 수정

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
-from app.db.routers import categories, stores
-from app.db.routers import menus  # 우리가 만든 라우터들 import
+from routers import stores
+from routers import menus
+from routers import categories  # 우리가 만든 라우터들 import
 
 app = FastAPI(
     title="VINUS Kiosk Backend API",
